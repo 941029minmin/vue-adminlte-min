1544d2be55f8ab8094af1bea79a96a33a0dcf184
Your Name <1787290283@qq.com>
2018-06-22 14:45:51 +0800

    word

--- src/page/smartSituation.vue
+++ src/page/smartSituation.vue
@@ -291,9 +291,9 @@
 	    	<div class="col-md-12">
 	    		<div class="box box-primary">
 	    			<div class="box-header with-border">
-             			<h3 class="box-title">抵消群体</h3>
+             			<h3 class="box-title">低消群体</h3>
               			<div class="box-tools pull-right">
-               				<button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+               				<button type="button" class="btn btn-box-tool" data-widget="col lapse"><i class="fa fa-minus"></i></button>
                 			<button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
              			</div>
             		</div>
